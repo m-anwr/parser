505dbbc0c4663b5fc2ada5e76aa2f8aff63b8f68
Shaza-Is <shaza993@yahoo.com>
2016-12-12 14:36:23 +0200

    adding EBNF grammar rules in comments

--- src/scanner/Parser.java
+++ src/scanner/Parser.java
@@ -16,4 +16,13 @@ public class Parser {
     //match  forwards the index *after the check*
     //unmatch  backwards the index to check another possibility
     // A method for each grammer rule after EBNF
+    // RULES:
+    // program ---> stmt_sequence
+    // stmt_sequence ---> statement {;statement}
+    // statement ---> if_statement | repeat_stmt | assign_stmt | read_stmt | write_stmt
+    // if_stmt ---> if exp then stmt_sequence [else stmt_sequence] end
+    // repeat_stmt ---> repeat stmt_sequence until exp
+    // assign_stmt ---> identifier := exp
+    // read_stmt ---> read identifier
+    // write_stmt ---> write exp
 }
