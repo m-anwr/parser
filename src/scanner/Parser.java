8ff564c802ba061dea0d11f369f889867d623f97
Shaza-Is <shaza993@yahoo.com>
2016-12-12 12:31:50 +0200

    the comments for parser idea

--- src/scanner/Parser.java
+++ src/scanner/Parser.java
@@ -11,4 +11,9 @@ package scanner;
  */
 public class Parser {
     
+    //the constructor takes an arraylist *SCANNER OUTPUT*
+    //Index data member starting from 0
+    //match  forwards the index *after the check*
+    //unmatch  backwards the index to check another possibility
+    // A method for each grammer rule after EBNF
 }
