694d0a3fa6be0c66479e7ed646baadc107295e0c
Shaza-Is <shaza993@yahoo.com>
2016-12-12 16:49:13 +0200

    constructor and data-members for parser

--- src/scanner/Parser.java
+++ src/scanner/Parser.java
@@ -5,14 +5,25 @@
  */
 package scanner;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author shaza
  */
 public class Parser {
+        
+    private final ArrayList<Pair<String, Scanner.TokenType>> scanOut;
+    private final int index;
     
     //the constructor takes an arraylist *SCANNER OUTPUT*
     //Index data member starting from 0
+
+    public Parser(ArrayList<Pair<String, Scanner.TokenType>> s)
+    {
+        scanOut = s;
+        index = 0;
+    }
     //match  forwards the index *after the check*
     //unmatch  backwards the index to check another possibility
     // A method for each grammer rule after EBNF
