2e1001e3a05febac6976ada5483f9438e3acf47e
Shaza-Is <shaza993@yahoo.com>
2016-12-12 16:34:51 +0200

    completing the comments for grammar rules

--- src/scanner/Parser.java
+++ src/scanner/Parser.java
@@ -17,12 +17,19 @@ public class Parser {
     //unmatch  backwards the index to check another possibility
     // A method for each grammer rule after EBNF
     // RULES:
-    // program ---> stmt_sequence
-    // stmt_sequence ---> statement {;statement}
+    // program --->         stmt_sequence
+    // stmt_sequence --->   statement {;statement}
     // statement ---> if_statement | repeat_stmt | assign_stmt | read_stmt | write_stmt
-    // if_stmt ---> if exp then stmt_sequence [else stmt_sequence] end
-    // repeat_stmt ---> repeat stmt_sequence until exp
-    // assign_stmt ---> identifier := exp
-    // read_stmt ---> read identifier
-    // write_stmt ---> write exp
+    // if_stmt --->         if exp then stmt_sequence [else stmt_sequence] end
+    // repeat_stmt --->     repeat stmt_sequence until exp
+    // assign_stmt --->     identifier := exp
+    // read_stmt --->       read identifier
+    // write_stmt --->      write exp
+    // exp --->             simple_exp [comparison_op simple_exp]
+    // comparison_op --->   < | =
+    // simple_exp --->      term {addop term}
+    // addop --->           +|-
+    // term --->            factor {mulop factor}
+    // mulop --->           * | /
+    // factor --->          (exp) | number | identifier
 }
